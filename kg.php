f77c0b92142f41b185c42067090ab94c39cf8059
Dave England <DavidEngland@hotmail.com>
2015-03-15 17:44:35 -0500

    Create kg.php

---
+++ kg.php
@@ -0,0 +1,14 @@
+<?php 
+/*
+	Plugin Name: Knowledge Graph
+	Plugin URI: https://github.com/DavidEngland/Knowledge-Graph
+	Description: Adds knowlege graph json-ld to WP.
+	Author: <a href="http://about.me/DavidEngland">Dave England</a>
+	Author URI: http://about.me/DavidEngland
+	Version: 0.0.1
+	License: GPL v2
+	Usage: Visit the plugin's settings page to configure your options.
+	Tags: knowledge-graph, head, wp_head, json-ld, ld-json, schema, microdata
+*/
+
+?>
